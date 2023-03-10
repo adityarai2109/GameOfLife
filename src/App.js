d7dd7ad3f19bedb8865851750e020c7919f46ae6
Aditya Rai <adityarai2109@gmail.com>
2023-03-10 16:56:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Game from "./Game";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [n, setN] = useState(15);
+	const init = () => {
+		let ans = [];
+		for (let i = 0; i < n; i++) {
+			let temp = [];
+			for (let j = 0; j < n; j++) temp.push(0);
+			ans.push(temp);
+		}
+		return ans;
+	};
+
+	const [grid, setGrid] = useState(init());
+	const [playing, setPlaying] = useState(0);
+
+	const habitable = (ng, i, j) => {
+		let nbr = 0;
+		for (let a = i - 1; a <= i + 1; a++) {
+			for (let b = j - 1; b <= j + 1; b++) {
+				if (a === i && b === j) continue;
+				if (a >= 0 && a < grid.length && b >= 0 && b < grid[0].length)
+					nbr += grid[a][b];
+			}
+		}
+		if (grid[i][j] === 1)
+			nbr < 2 || nbr > 3 ? (ng[i][j] = 0) : (ng[i][j] = 1);
+		else nbr === 3 ? (ng[i][j] = 1) : ([i][j] = 0);
+	};
+
+	const life = () => {
+		let ng = init();
+		for (let i = 0; i < grid.length; i++)
+			for (let j = 0; j < grid[0].length; j++) habitable(ng, i, j);
+		setGrid(ng);
+	};
+
+	return (
+		<div className="App">
+			<Game n={n} grid={grid} setGrid={setGrid} playing={playing} />
+			<button
+				onClick={() => {
+					life();
+				}}
+			>
+				Start/Stop
+			</button>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Game.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+export default function Game(props) {
+	const handleClick = (i, j) => {
+		let nd = [...props.grid];
+		nd[i][j] = 1 - nd[i][j];
+		props.setGrid(nd);
+	};
+
+	return (
+		<div
+			className="grid1"
+			style={{
+				display: "grid",
+				width: "10rem",
+				gridTemplateColumns: `repeat(${props.n},1fr)`,
+				gridTemplateRows: `repeat(${props.n},1fr)`,
+			}}
+		>
+			{props.grid.map((row, i) =>
+				row.map((cell, j) => (
+					<button
+						onClick={() => {
+							handleClick(i, j);
+						}}
+						className={cell === 1 ? "clicked" : "unclicked"}
+						style={{ height: "40px", width: "40px" }}
+					>
+						{cell}
+					</button>
+				))
+			)}
+		</div>
+	);
+}
